876c9a3b1e8dedda2f339ff4f50bdbe96da0f19b
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-08 17:34:44 +0900

    feat: 야구 게임 시작 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.domain.BaseballGame;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        do {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.start();
+        } while (BaseballGame.wantRestart());
+        OutputView.printEndGame();
     }
 }
